22f1eafdaf12867e5b466d9e805ad449fb470fda
chaosishere <148432485+chaosishere@users.noreply.github.com>
2024-01-21 03:00:05 +0530

    Adding endpoints to our User models

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
 
     path('',views.home,name='home'),
     path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view())
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from rest_framework import generics
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 # Create your views here.
 
 # Generics Views for API :-
@@ -15,7 +16,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+class UserList(generics.ListAPIView):  # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
+
+class UserDetail(generics.RetrieveAPIView):  # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # Normal URL views:-
 def home(request):
