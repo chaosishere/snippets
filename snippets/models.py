46a435384119a3efa08c7e08204def45876df02e
chaosishere <148432485+chaosishere@users.noreply.github.com>
2024-01-21 02:29:24 +0530

    fixed the migrations and models.py

--- snippets/models.py
+++ snippets/models.py
@@ -1,30 +1,44 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments import highlight  # new
+from pygments.formatters.html import HtmlFormatter  # new
+from pygments.lexers import get_all_lexers, get_lexer_by_name  # new
 from pygments.styles import get_all_styles
 
-
-#pygments :-
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
-
-
-# Create your models here.
 
 class Snippet(models.Model):
-    title = models.CharField(max_length=200, blank=True, default="")
-    code = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    lineos = models.BooleanField(default=False)
+    title = models.CharField(max_length=100, blank=True, default="")
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
     language = models.CharField(
-        choices= LANGUAGE_CHOICES, default="python", max_length=100
+        choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
+    owner = models.ForeignKey(
+        "auth.User", related_name="snippets", on_delete=models.CASCADE
+    )  # new
+    highlighted = models.TextField()  # new
 
     class Meta:
-        ordering = ['created']
+        ordering = ("created",)
 
+    def save(self, *args, **kwargs):  # new
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
