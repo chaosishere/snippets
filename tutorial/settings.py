6025bc2ba2d770ce8096edb14a2d1962148db529
chaosishere <148432485+chaosishere@users.noreply.github.com>
2024-01-25 02:40:48 +0530

    Migrated to PostgreSQL

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project_database',
+        'USER': 'postgres',
+        'PASSWORD': 'chaos789',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
